9dbb9c1a4172465380edefb86e34f05c0a1bfd35
Ben Hammen <ben.hammen@alliancels.com>
2016-12-01 15:24:43 -0600

    Add IAR ARM sample project

---
+++ subprojects/docs/src/samples/native-binaries/iar-arm/src/main/asm/sum.s
@@ -0,0 +1,8 @@
+    rseg CODE:CODE(2)
+
+    export sum
+sum
+    add r0, r1, r0
+    bx lr
+
+    end
---
+++ subprojects/docs/src/samples/native-binaries/iar-arm/src/main/c/main.c
@@ -0,0 +1,10 @@
+#include "hello.h"
+#include "sum.h"
+#include <stdio.h>
+
+int main () {
+  hello();
+  printf("%i", sum(1, 1));
+
+  return 0;
+}
